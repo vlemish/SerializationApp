afa714c9fec72e0fb7b4029588ae6c8feea18179
Vladislav Lemish <vlad.lemish123@gmail.com>
2021-03-18 22:51:11 +0200

    Add project files.

---
+++ SerializationApp/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.IO;
+
+using SerializationApp.Serializers;
+
+namespace SerializationApp
+{
+    class Program
+    {       
+        static void Main(string[] args)
+        {
+            try
+            {
+                if (args.Length < 3 || args.Length > 4)
+                {
+                    var msg = args.Length < 3
+                        ? "Not enough arguments!"
+                        : "Too much arguments!";
+                    throw new ArgumentOutOfRangeException(msg);
+                }
+
+                Serializer serializer;
+                var type = ParseArg(args[0].ToLower(),
+                    arg => arg.Equals("binary") || arg.Equals("xml"),
+                    "Wrong value for a type!");
+                var operation = ParseArg(args[1].ToLower(),
+                    arg => arg.Equals("ser") || arg.Equals("des"),
+                    "Wrong value for an operation");
+                var filePath = ParseArg(args[2].ToLower(),
+                    arg => Serializer.IsValidFilePath(arg), 
+                    "Wrong file path!");
+
+                switch (operation)
+                {
+                    case "ser":
+                        {
+                            if (args.Length < 4)
+                            {
+                                throw new ArgumentNullException("Can't serialize a structure without given path to the directory!");
+                            }
+                            var directoryPath = ParseArg(args[3],
+                                arg => Directory.Exists(arg),
+                                "Wrong directory path!");
+                            serializer = SerializerStaticFactory.CreateSerializer(type, filePath, directoryPath);
+                            serializer.Serialize();
+                            Console.WriteLine("Structure was successfully serialized!");
+                            break;
+                        }
+                    case "des":
+                        {
+                            serializer = SerializerStaticFactory.CreateSerializer(type, filePath);
+                            var structure = serializer.Deserialize();
+                            Console.WriteLine("Your stucture:\n" + structure.ToString());
+                            Console.WriteLine("Structure was successfully deserialized!");
+                            break;
+                        }
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(ex.Message);          
+                Console.WriteLine("\nTo serialize a directory, you should specify a type of serializer, an operation (ser - to serialize)," +
+                    "a path to file where the structure of the directory will be saved, and the path to the directory itself." +
+                    "\n\nType can be xml or binary. Operation can be ser(serialize) or des(deserialize).\n" +
+                    "\nExample of use: binary ser F:\\Data\\source.txt F:\\Data\n" +
+                    "\nTo deserialize a structure, you can drop out the path to the directory and specify the type of serializer (the one that was used to serialize), " +
+                    "an operation (des), and a path to the file in which the structure of the file stored.\n" +
+                    "\nExample of use: binary des F:\\Data\\source.txt\n" +
+                    "\nThere is a strict order of arguments in the program. The order of args should be next: type-> operation-> file path-> directory path (if deserializing).\n");
+                Console.ResetColor();
+            }
+        }   
+        
+        static string ParseArg(string arg, Func<string, bool> condition, string erorMsg)
+        {            
+            return condition(arg)
+                ? arg
+                : throw new ArgumentException(erorMsg);
+        }
+    }
+}
---
+++ SerializationAppTests/SerializerTests.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.IO;
+
+using NUnit.Framework;
+
+using SerializationApp.Components;
+using SerializationApp.Serializers;
+
+namespace SerializationAppTests
+{
+    [TestFixture]
+    public class SerializerTests
+    {
+        private DirectoryComponent expected;
+
+        private string filePath;
+
+
+        [Test]
+        [TestCase("binary", "file.txt")]
+        [TestCase("xml", "file.txt")]      
+        public void SerializeDeserializeTests(string type, string filePath)
+        {
+            this.filePath = filePath; 
+
+            CreateRealDirectory(expected, string.Empty);
+            Serializer serializer = SerializerStaticFactory.CreateSerializer(type, filePath, expected.Name);
+            serializer.Serialize();
+            var actual = serializer.Deserialize();
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [OneTimeSetUp]
+        public void SetUp()
+        {
+            var currentPath = Directory.GetCurrentDirectory();
+
+            var root = new DirectoryComponent() { Name = $"{currentPath}\\TestDirectory", Depth = 0, IsLeaf = false, IsRoot = true };
+            root.Components.Add(new FileComponent("txt1", 0, 0, DateTime.Now, FileAttributes.Archive, false));
+            root.Components.Add(new FileComponent("bin1", 0, 0, DateTime.Now, FileAttributes.Archive, false));
+            root.Components.Add(new FileComponent("xml1", 0, 0, DateTime.Now, FileAttributes.Archive, false));
+
+            var nested1 = new DirectoryComponent() { Name = "Nested1", Depth = 1, IsLeaf = false };
+            nested1.Components.Add(new FileComponent("txt2", 0, 1, DateTime.Now, FileAttributes.Archive, false));
+            nested1.Components.Add(new FileComponent("bin2", 0, 1, DateTime.Now, FileAttributes.Archive, false));
+            nested1.Components.Add(new FileComponent("xml2", 0, 1, DateTime.Now, FileAttributes.Archive, false));
+
+            var nested11 = new DirectoryComponent() { Name = "Nested11", Depth = 2, IsLeaf = true };
+            nested11.Components.Add(new FileComponent("nestedFile1", 0, 2, DateTime.Now, FileAttributes.Archive, true));
+
+            nested1.Components.Add(nested11);
+            root.Components.Add(nested1);
+
+            var nested2 = new DirectoryComponent() { Name = "Nested2", Depth = 1, IsLeaf = true };
+            nested2.Components.Add(new FileComponent("txt2", 0, 1, DateTime.Now, FileAttributes.Archive, false));
+            nested2.Components.Add(new FileComponent("bin2", 0, 1, DateTime.Now, FileAttributes.Archive, false));
+            nested2.Components.Add(new FileComponent("xml2", 0, 1, DateTime.Now, FileAttributes.Archive, false));
+
+            var nested22 = new DirectoryComponent() { Name = "Nested22", Depth = 2, IsLeaf = true };
+            nested22.Components.Add(new FileComponent("nestedFile1", 0, 2, DateTime.Now, FileAttributes.Archive, true));
+
+            nested2.Components.Add(nested22);
+            root.Add(nested2);
+
+            expected = root;
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            //delete created file and directory to avoid trash holding   
+            if (File.Exists(filePath))
+                File.Delete(filePath);
+
+            if(Directory.Exists(expected.Name))
+                Directory.Delete(expected.Name, true);
+        }
+
+        private void CreateRealDirectory(DirectoryComponent directory, string parentPath)
+        {
+            var path = String.IsNullOrEmpty(parentPath)
+                ? directory.Name
+                : parentPath + @"\" + directory.Name;
+
+            var root = Directory.CreateDirectory(path);
+            foreach (var item in directory.Components)
+            {
+                if (item is DirectoryComponent)
+                    CreateRealDirectory(item as DirectoryComponent, root.FullName);
+                else
+                {
+                    var temp = item as FileComponent;
+
+                    var file = new FileInfo(root.FullName + @"\" + item.Name);
+                    file.Create().Dispose();
+                    file.CreationTime = temp.CreationTime;
+                    file.Attributes = temp.Attributes;
+                }
+            }
+        }   
+    }   
+}
